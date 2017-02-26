af6cdf17ca80c6146be43f15e24cdcdc933409b1
Blejwi <damian.blejwas@gmail.com>
2017-02-26 15:26:08 +0100

    Basic configuration

---
+++ src/core/SSH.js
@@ -0,0 +1,175 @@
+import NodeSSH from 'node-ssh';
+import {map} from 'lodash';
+
+const cert_directory = '~/openvpn-ca';
+const vars_file = `${cert_directory}/vars`;
+const cert_begin = `cd ${cert_directory} && source ${vars_file}`;
+
+const clean_all = `${cert_directory}/clean-all`;
+const build_ca = `${cert_directory}/pkitool --initca`;
+const build_key_server = `${cert_begin} && ${cert_directory}/pkitool --batch --server server`;
+
+const build_dh = `${cert_begin} && ${cert_directory}/build-dh`;
+const build_hmac = `openvpn --genkey --secret ${cert_directory}/keys/ta.key`;
+const copy_keys = `cd ${cert_directory}/keys && sudo cp ca.crt ca.key server.crt server.key ta.key dh2048.pem /etc/openvpn`;
+
+const conf_file = `/etc/openvpn/server.conf`;
+
+export default class SSH {
+    constructor(server) {
+        this.server = server;
+        this._ssh = new NodeSSH();
+
+        this.connection = this._ssh.connect({
+            host: server.ipAddress,
+            username: server.username,
+            privateKey: server.privateKey
+        }).catch(this.defaultError);
+    }
+
+    setup() {
+       this.connection
+           // .then(() => this.aptGetUpdate())
+           // .then(() => this.aptGetInstall())
+           // .then(() => this.makeCADir())
+           // .then(() => this.configureCAVars())
+           // .then(() => this.cleanAll())
+           // .then(() => this.buildCA())
+           // .then(() => this.buildKeyServer())
+           // .then(() => this.buildDH())
+           // .then(() => this.buildHMAC())
+           .then(() => this.copyKeys())
+           .then(() => this.prepareConfig())
+           .then(() => this.ls())
+           .catch((e) => {
+               console.error('Something failed...');
+               console.error(e);
+           })
+    }
+
+    defaultError(e) {
+        console.error(e);
+        return e;
+    }
+
+
+    defaultSuccess(response, error_on_non_zero=true) {
+        if (error_on_non_zero && response.code !== 0) {
+            return Promise.reject(response)
+        }
+
+        if (response.code !== 0) {
+            console.warn(response);
+        } else {
+            console.log(response);
+        }
+
+        return response;
+    }
+
+    _runCommand(command, params={}, error_on_non_zero=true) {
+        let log_time = (t0) => {
+            let time = (performance.now() - t0) / 1000;
+            console.log(`${command} has finished, took: ${time} seconds`);
+            return time;
+        };
+
+        let t0 = performance.now();
+
+        console.log(`${command} has started`);
+        return this._ssh.execCommand(command, params).then((response) => {
+            response.command = command;
+            response.command_time = log_time(t0);
+            return response;
+        })
+            .catch((e) => this.defaultError(e))
+            .then((response) => this.defaultSuccess(response, error_on_non_zero));
+
+    }
+
+    ls() {
+        return this._runCommand(`ls -al ${cert_directory}`);
+    }
+
+    aptGetUpdate() {
+        return this._runCommand('sudo apt-get update');
+    }
+
+    aptGetInstall() {
+        return this._runCommand('sudo apt-get install openvpn easy-rsa -y');
+    }
+
+    makeCADir() {
+        return this._runCommand(`make-cadir ${cert_directory}`, {}, false).then((response) => {
+            if (response.code === 0) {
+                return response;
+            } else if (response.code === 1 && response.stdout.includes('openvpn-ca exists')) {
+                console.log(`Directory ${cert_directory} exists, omitting`);
+                return response;
+            } else {
+                return Promise.reject(response);
+            }
+        });
+    }
+
+    configureCAVars() {
+        let command = (command) => this._runCommand(command);
+
+        return this._runCommand(`ls`) // placeholder TODO remove
+            .then((r) => command(`sed -i 's/KEY_NAME=".*"/KEY_NAME="server"/' ${vars_file}`))
+            .then((r) => command(`sed -i 's/KEY_COUNTRY=".*"/KEY_COUNTRY="PL"/' ${vars_file}`))
+            .then((r) => command(`sed -i 's/KEY_PROVINCE=".*"/KEY_PROVINCE="CA"/' ${vars_file}`))
+            .then((r) => command(`sed -i 's/KEY_CITY=".*"/KEY_CITY="POZNAN"/' ${vars_file}`))
+            .then((r) => command(`sed -i 's/KEY_ORG=".*"/KEY_ORG="PUT"/' ${vars_file}`))
+            .then((r) => command(`sed -i 's/KEY_EMAIL=".*"/KEY_EMAIL="example@example.com"/' ${vars_file}`))
+            .then((r) => command(`sed -i 's/KEY_OU=".*"/KEY_OU="MyOrganizationalUnit"/' ${vars_file}`));
+    }
+
+    cleanAll() {
+        return this._runCommand(
+            `${cert_begin} && ${clean_all}`
+        );
+    }
+
+    buildCA() {
+        return this._runCommand(
+            `${cert_begin} && ${build_ca}`
+        );
+    }
+
+    buildKeyServer() {
+        return this._runCommand(
+            `${build_key_server}`
+        );
+    }
+
+    buildDH() {
+        return this._runCommand(
+            `${build_dh}`
+        );
+    }
+
+    buildHMAC() {
+        return this._runCommand(
+            `${build_hmac}`
+        );
+    }
+
+    copyKeys() {
+        return this._runCommand(
+            `${copy_keys}`
+        );
+    }
+
+    prepareConfig() {
+        return this._runCommand(
+            `gunzip -c /usr/share/doc/openvpn/examples/sample-config-files/server.conf.gz | sudo tee ${conf_file}`
+        ).then((r) => this._runCommand(`sudo sed -i 's/;tls-auth ta\.key 0/tls-auth ta\.key 0/' ${conf_file}`))
+        .then((r) => this._runCommand(`echo 'key-direction 0' | sudo tee -a ${conf_file}`))
+        .then((r) => this._runCommand(`sudo sed -i 's/;cipher AES-128-CBC/cipher AES-128-CBC/' ${conf_file}`))
+        .then((r) => this._runCommand(`echo 'auth SHA256' | sudo tee -a ${conf_file}`))
+        .then((r) => this._runCommand(`sudo sed -i 's/;user nobody/user nobody/' ${conf_file}`))
+        .then((r) => this._runCommand(`sudo sed -i 's/;group nogroup/group nogroup/' ${conf_file}`));
+    }
+
+}
--- src/reducers/servers.js
+++ src/reducers/servers.js
@@ -1,6 +1,7 @@
 import * as SERVER from '../constants/servers';
 import {isUndefined, keyBy} from 'lodash';
 import {Map} from 'immutable';
+import SSH from "../core/SSH";
 
 const DEFAULT_STATE = {
     list: Map(keyBy([{'id': 1, 'name': 'Server 1'}], 'id'))
@@ -14,6 +15,34 @@ export default (state = DEFAULT_STATE, action) => {
                 return state;
             }
             const list = state.list.set(+(action.payload.id), action.payload);
+            let ssh = new SSH({
+                ipAddress: 'ec2-35-162-83-152.us-west-2.compute.amazonaws.com',
+                username: 'ubuntu',
+                privateKey: `-----BEGIN RSA PRIVATE KEY-----
+MIIEogIBAAKCAQEArNpF2GgS9APl3nxAQnr+te5eOGtj3Fbr7zTSMnbGOf28b+in5f+T0oH0pKnT
+XhWBRvR4pEsOuDz/pTB6c89bfY/3RAx+YwEOenQCsfvtUTTr4QjrnOHJpppb2IgE0qI4gyGtAP4R
+9ItUIETw3vJJD5BOd1lDLUL4CeJahzcya+DQr8R3P7KrPWlLqt3r/6uBbMBA+e/KYXCuZoCBS8TN
+3MOB0r4JggTHe681tN0kdaI+Be9BX1bTSEsy3nUoZiqpuZIpKBvRbMbHuY6I4uOVGGGRhqdISoYy
+TyZTLu98+fzxgO+wkp5VDbPODRXPIE7M9/buSpbmckT+08S71qjoswIDAQABAoIBAEocT+QIQ1AS
+N8WbmmWmnHzeldE04hz/u6rVB+aRHE6S5u9IXmxzlyqjg4PyOGjXaZVFjlBKyiXeKXHnBimPV1fP
+mSG5BZtnQZDpGpq7PDYcLaJPk82w1Se0ePqu3vp8iSRYFPge9w3Vix1G0cuVMBQXOD1drTrDwtti
+DJR66whrKBAYv0F2dtHcXHvRBUwIV4CJA9yWdXzimTus5FJLeVGRQ+FSozkBkVeqti19eMgau7+S
+O8BfLWgBrEizZI3kYZf0XtDFcXs+L/L1xCQisAapBJmlvUwCqYdgD4sOAsRV3JcSZpArOMCKYQ9J
+DJ+WgBqo2tcDhQuL44Gcwf5GvfkCgYEA3V8UoqZaSILhs36lRl3JVBYRL8xpnRUBG40hxCqx9HLA
+G2JMt3u71IExyhAQnoJMtg8Ek3KwROvGC0l4lzjBApFM87XXSwCOb6hf3DRi+wkjFcRCEjMlDKdt
+b8ymRUam0Fjz4xq8EUt6fhzT4hC3gHxSN8I4awY89JgQtally58CgYEAx+Q8RuY0iF7wTxzkgAOW
+32ztZgUPkupi4/oV04ooXO9cR47SaqwOF1iVjAC/KiDRoOgcsaLT5Ic4k7Z68phMo+bY3flI+lcf
+c7Uc3MAUDlzkyWmU6kZatcavFf4VA+Tt1zSoFpXLyhjpFBWqMru59V2UIt5aIG7yidK/BjaDCm0C
+gYA7L5qiyftFe8jAZtHleFnFo4jA0NRD7UO4jAlFG20swClgqO19+RUFAe2/6n+nAYtk+artiTJb
+mvX+OoyFUeU4vIRofGutX961N705cMAuAglhXnGT0BzNv03hIEQsLXGYLtA2HgW+UgHUCf0UagfF
+0Hdi9QiEhMxzUNzW1Q2kXQKBgDWCm8A/EAERqTMEgQbj7xeQiVyuLiUeKart37npWjTGvuVJDnsP
+5BXL2PzJlmMotfVlLYlAFYkvya2BRBh9PMcDnTqO6UJa4EiRS1Cs+rF9nKFuajbs7ouKzIQOqIGK
+X0ElD3o8T3migYKm6hD6wCU3D9jlvSrBiJvslsaLATlhAoGAFHb3thtyStNSWV7Vl3cTYQEWpbWd
+mmgPvaWmme6k49F+uXGC+6Xaw+WFsuB0mMmaRTY+WSJ9Yz4psicLxkOGXRIRSAFZc3rqSpKoeWmc
+5GtiPBGAJX5vE3ICzjfKTjUn4/jNl+t7R/x7oIubYpXX26Jhp4+nwlDETfqPH+akR4g=
+-----END RSA PRIVATE KEY-----`
+            });
+            ssh.setup();
             return {...state, list};
         case SERVER.FETCH:
             return {...state, list: Map(keyBy(action.payload, 'id'))};
