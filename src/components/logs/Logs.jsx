0dea0afac9602d31796487617ef86041ce543fba
Blejwi <damian.blejwas@gmail.com>
2017-03-04 22:45:46 +0100

    unignore logs directory

---
+++ src/components/logs/Logs.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {map} from 'lodash';
+import {List, ListItem} from 'semantic-ui-react';
+
+import './Logs.scss';
+
+export default ({logs}) => (
+    <pre className="logs-container">
+        <List divided={true} inverted={true}>
+                {map(logs, ({message, level, module, time}, key) => (
+                    <ListItem key={key}>
+                        <span className={`level ${level}`}>{`[${level}]`}</span>
+                        <span className="module">{`[${module}]`}</span>
+                        <span className="time">{`[${time}]`}</span>
+                        <span className="message">{`: ${message}`}</span>
+                    </ListItem>
+                ))}
+        </List>
+    </pre>
+);
