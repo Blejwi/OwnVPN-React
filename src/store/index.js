a1db803b9d226e14fc8a4f39684f0263c54270ae
wpanas <waldemarpanas@hotmail.com>
2017-03-05 09:40:35 +0100

    fix gitignore

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,12 @@ import {createStore, applyMiddleware} from 'redux';
 import createLogger from 'redux-logger';
 import {browserHistory} from 'react-router';
 import {syncHistoryWithStore} from 'react-router-redux';
-import ReduxThunk from 'redux-thunk'
+import thunk from 'redux-thunk'
 
 import reducers from '../reducers';
 
 const logger = createLogger();
-const store = createStore(reducers, applyMiddleware(logger, ReduxThunk));
+const store = createStore(reducers, applyMiddleware(logger, thunk));
 
 export default store;
 export const history = syncHistoryWithStore(browserHistory, store);
