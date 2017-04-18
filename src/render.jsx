fe8c7d3dbb2fedb03fbbb9defe0af346557e764e
wpanas <waldemarpanas@hotmail.com>
2017-04-18 18:11:25 +0200

    restore previous reload

--- src/render.jsx
+++ src/render.jsx
@@ -1,21 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {AppContainer} from 'react-hot-loader';
 import App from './containers/App';
 import 'semantic-ui-css/semantic.min.css';
 import 'react-redux-toastr/src/styles/index.scss';
 import './resource/main.scss';
 
-const root = document.getElementById("main");
-const render = Component => ReactDOM.render(
-    <AppContainer>
-        <Component/>
-    </AppContainer>,
-    root
-);
-
-render(App);
-
-if (module.hot) {
-    module.hot.accept('./containers/App', () => render(App));
-}
+ReactDOM.render(<App/>, document.getElementById('main'));
