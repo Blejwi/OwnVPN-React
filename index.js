03bd726029125c1fbe12550dca3aef669f2e6057
Blejwi <damian.blejwas@gmail.com>
2017-04-27 23:26:51 +0200

    Fix for ESlint problems

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
+/* eslint-disable import/no-extraneous-dependencies */
 const electron = require('electron');
 const developerTools = require('electron-devtools-installer');
-const app = electron.app;
 const electronDebug = require('electron-debug');
 const electronContextMenu = require('electron-context-menu');
+
+const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
+
 let mainWindow = null;
 
-app.on('window-all-closed', () => (process.platform !== 'darwin')?app.quit():null);
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        return app.quit();
+    }
+    return null;
+});
 
-electronDebug({showDevTools: true});
+electronDebug({ showDevTools: true });
 electronContextMenu();
 
 app.on('ready', () => {
     developerTools.default(developerTools.REACT_DEVELOPER_TOOLS);
     developerTools.default(developerTools.REDUX_DEVTOOLS);
-    mainWindow = new BrowserWindow({width: 1024, height: 728});
+    mainWindow = new BrowserWindow({ width: 1024, height: 728 });
     mainWindow.loadURL('http://localhost:8080/');
-    mainWindow.on('closed', () => mainWindow = null);
+    mainWindow.on('closed', () => (mainWindow = null));
 });
